f5c747e2c3c28b7f21a1868c80b79357d5544fb6
jonathantsang <j26tsang@gmail.com>
2018-01-20 23:33:56 -0500

    start

--- app.js
+++ app.js
@@ -101,5 +101,5 @@ app.post("/concept", function (req, res) {
 });
 
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
 
